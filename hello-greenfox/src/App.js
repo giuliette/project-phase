0ec5eeebb91476b2238e8e013a3df4be2001831c
Juli Domány <domanyjuli@gmail.com>
2019-01-16 17:09:52 +0100

    feat(golden-acorn-app):

--- hello-greenfox/src/App.js
+++ hello-greenfox/src/App.js
@@ -1,25 +1,22 @@
 import React, { Component } from 'react';
-import Buttons from './Button';
-import Display from './counter';
+import Button from './Button';
+import Display from './Display';
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      counter: 0
-    }
+  state = {
+    counter: 0
   }
 
   increase = () => {
     this.setState({
-      clicks: this.state.clicks + 1
+      counter: this.state.counter + 1
     });
   }
 
   decrease = () => {
     this.setState({
       counter: this.state.counter > 0 ? --this.state.counter : this.state.counter
-    })
+    });
   }
 
   keyWatchers(e) {
@@ -30,40 +27,16 @@ class App extends Component {
     keyActionPairs[e.keyCode] ? keyActionPairs[e.keyCode]() : () => { };
   }
 
-  // keyPress = e =>
-  //   (this.keyActionPairs[e.keyCode]) ? this.keyActionPairs[e.keyCode]() : () => { };
-
-  componentDidMount = () => {
-    document.addEventListener('keydown', this.keyPress);
-  }
-
+  // keyPress = e => (this.keyActionPairs[e.keyCode]) ? this.keyActionPairs[e.keyCode]() : () => { };
   render() {
-    const { counter } = this.state;
     return (
       <div>
-        <h1>simple golden acorn app</h1>
-        <button functions={
-          {
-            add: this.add,
-            eat: this.eatOne,
-            keyWatchers: this.keyWatchers
-          }
-        }>
-        </button>
+        <Button clickEvent={this.increase} name='get one 🌰'/>
+        <Display amount={this.state.counter}/>
+        <Button clickEvent={this.decrease} name='eat one 🌰' />
       </div>
     )
   }
-
-  // render() =>
-
-  //   (
-  //     <div>
-  //       <h1>Simple Golden Acorn App</h1>
-  //       <Buttons name="Buy One" onClick={this.increase} />
-  //       <Display counter={this.state.clicks} />
-  //       <Buttons name="Eat One" onClick={this.decrease} />
-  //     </div>
-  //   )
-};
+}
 
 export default App;
