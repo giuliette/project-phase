3931fac5836912cee93c302a3511e990ec512ac0
Juli Domány <domanyjuli@gmail.com>
2019-01-18 15:34:29 +0100

    feat(Router): home, states, redux components added, router added

---
+++ hello-greenfox/src/WithRedux.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import App from './App';
+
+const withRedux = () => (
+  <div>
+    <h1> Golden Acorn is life! 🌰 </h1>
+    <App />
+  </div>
+)
+
+export default withRedux;
